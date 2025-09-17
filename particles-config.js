317c5a77ec0ce8eb2b6f9cd63299d87629453381
Touhidur Rahman <rahmantouhidor@gmail.com>
2025-09-17 15:46:12 +0600

    updated particle effects and 3d cubes responsiveness

---
+++ particles-config.js
@@ -0,0 +1,136 @@
+// Vincent Garreau particles.js configuration with mouse interaction
+// Based on https://vincentgarreau.com/particles.js/
+
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 80,
+      "density": {
+        "enable": true,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": "circle",
+      "stroke": {
+        "width": 0,
+        "color": "#000000"
+      },
+      "polygon": {
+        "nb_sides": 5
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.5,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 3,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 40,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 150,
+      "color": "#ffffff",
+      "opacity": 0.4,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 6,
+      "direction": "none",
+      "random": false,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": false,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "canvas",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "repulse"
+      },
+      "onclick": {
+        "enable": true,
+        "mode": "push"
+      },
+      "resize": true
+    },
+    "modes": {
+      "grab": {
+        "distance": 400,
+        "line_linked": {
+          "opacity": 1
+        }
+      },
+      "bubble": {
+        "distance": 400,
+        "size": 40,
+        "duration": 2,
+        "opacity": 8,
+        "speed": 3
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 4
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
+
+// Responsive particle count for performance
+function updateParticleCount() {
+  const width = window.innerWidth;
+  let particleCount = 80;
+  
+  if (width < 768) {
+    particleCount = 30;
+  } else if (width < 1024) {
+    particleCount = 50;
+  }
+  
+  if (window.pJSDom && window.pJSDom[0] && window.pJSDom[0].pJS) {
+    window.pJSDom[0].pJS.particles.number.value = particleCount;
+    window.pJSDom[0].pJS.fn.particlesRefresh();
+  }
+}
+
+// Update on resize
+window.addEventListener('resize', updateParticleCount);
+
+// Initial update
+setTimeout(updateParticleCount, 100);
